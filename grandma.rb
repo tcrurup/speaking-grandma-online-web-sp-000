f5a4d5dc54026e7107ceeb9a39a3bd1754ba6fb2
Anthony Rurup <tcrurup@gmail.com>
2019-04-24 18:59:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    "NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
 
