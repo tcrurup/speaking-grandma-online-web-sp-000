c69d3c156faab7787c904a8f3e333b8e027355ee
Anthony Rurup <tcrurup@gmail.com>
2019-04-24 18:58:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA"
-    "I LOVE YOU TOO PUMPKIN!"
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
 
